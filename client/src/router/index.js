2682d86652355a03f53bcbce2982ef04931c5f48
Aayush Mishra <aayushmishra@himalayacollege.edu.np>
2026-06-04 12:07:05 +0545

    refactor(WIP): updated the home and about station in storefront

--- client/src/router/index.js
+++ client/src/router/index.js
@@ -1,23 +1,61 @@
 import { createRouter, createWebHistory } from 'vue-router'
+
 import Home from '../views/Home.vue'
+import FindStation from '../views/FindStation.vue'//
 import About from '../views/About.vue'
+import AdminLogin from '../views/AdminLogin.vue'
+import AdminDashboard from '../views/AdminDashboard.vue'
 
 const routes = [
   {
     path: '/',
     name: 'Home',
-    component: Home,
+    component: Home
   },
+
+  {
+    path: '/find',
+    name: 'FindStation',
+    component: FindStation
+  },
+
   {
     path: '/about',
     name: 'About',
-    component: About,
+    component: About
   },
+
+  {
+    path: '/admin-login',
+    name: 'AdminLogin',
+    component: AdminLogin
+  },
+
+  {
+    path: '/admin/dashboard',
+    name: 'AdminDashboard',
+    component: AdminDashboard,
+    meta: { requiresAdmin: true }
+  }
 ]
 
 const router = createRouter({
   history: createWebHistory(),
-  routes,
+  routes
+})
+
+router.beforeEach((to, from, next) => {
+  if (to.meta.requiresAdmin) {
+    const isAdmin = localStorage.getItem('adminAuth')
+
+    if (!isAdmin) {
+      next('/admin-login')
+    } else {
+      next()
+    }
+  } else {
+    next()
+  }
 })
 
 export default router;
